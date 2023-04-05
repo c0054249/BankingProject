b6053a38f99fa8d243b662241e9abe66b04eafac
c0054349 <c0054249@newcastle.ac.uk>
2023-04-05 18:32:07 +0100

    Add accessibility and application feature questions to receive user input for the search algorithm

--- search_algorithm/views.py
+++ search_algorithm/views.py
@@ -10,22 +10,45 @@ def search_home():
 
 @search_algorithm_blueprint.route('/services')
 def services():
-    return render_template('search_services.html')
+    return render_template('search.html')
 
 
 @search_algorithm_blueprint.route('/submit', methods=['POST'])
 def submit():
+    # services responses
     current_account = request.form['current_account']
     savings_account = request.form['savings_account']
     credit_card = request.form['credit_card']
     isa = request.form['isa']
     mortgage = request.form['mortgage']
 
-    # Do something with the user's answers here, like store them in a database or print them out
+    # accessibility responses
+    branches = int(request.form['branches'])
+    withdrawalLimit = int(request.form['withdrawalLimit'])
+    online_services = request.form['online_services']
+    mobile_services = request.form['mobile_services']
+
+    # application features
+    freeze_card = request.form['freezeCard']
+    instant_notifications = request.form['instantNotifications']
+    spending_categories = request.form['spendingCategories']
+    turn_off_spending = request.form['turnOffSpending']
+    spending_goals = request.form['spendingGoals']
+
     print(f"User selected {current_account} for current account")
     print(f"User selected {savings_account} for savings account")
     print(f"User selected {credit_card} for credit card")
     print(f"User selected {isa} for ISA")
     print(f"User selected {mortgage} for mortgage")
+    print(f"User selected {branches} for number of branches")
+    print(f"User selected {withdrawalLimit} for maximum withdrawal limit from atm's")
+    print(f"User selected {online_services} for online services")
+    print(f"User selected {mobile_services} for mobile services")
+    print(f"User selected {freeze_card} for freeze card")
+    print(f"User selected {instant_notifications} for instant notifications")
+    print(f"User selected {spending_categories} for spending categories")
+    print(f"User selected {turn_off_spending} for turn off certain spending")
+    print(f"User selected {spending_goals} for spending goals")
+
 
     return "Answers submitted successfully!"
