67d92a8b08ee1b36f3a8f72e2ccebab9836c2b3a
c0054349 <c0054249@newcastle.ac.uk>
2023-04-05 16:28:12 +0100

    Create the base for obtaining user inputted answers for querying against the database

--- search_algorithm/views.py
+++ search_algorithm/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 search_algorithm_blueprint = Blueprint('search_algorithm', __name__, template_folder='templates')
 
@@ -10,4 +10,22 @@ def search_home():
 
 @search_algorithm_blueprint.route('/services')
 def services():
-    return
+    return render_template('search_services.html')
+
+
+@search_algorithm_blueprint.route('/submit', methods=['POST'])
+def submit():
+    current_account = request.form['current_account']
+    savings_account = request.form['savings_account']
+    credit_card = request.form['credit_card']
+    isa = request.form['isa']
+    mortgage = request.form['mortgage']
+
+    # Do something with the user's answers here, like store them in a database or print them out
+    print(f"User selected {current_account} for current account")
+    print(f"User selected {savings_account} for savings account")
+    print(f"User selected {credit_card} for credit card")
+    print(f"User selected {isa} for ISA")
+    print(f"User selected {mortgage} for mortgage")
+
+    return "Answers submitted successfully!"
