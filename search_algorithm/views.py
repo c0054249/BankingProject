41ca1a0843aa375c1594837b9573eaa19a60f9db
c0054349 <c0054249@newcastle.ac.uk>
2023-04-10 10:33:39 +0100

    Add html formatting to show results for the top rated service percentage match from views.py

--- search_algorithm/views.py
+++ search_algorithm/views.py
@@ -289,4 +289,5 @@ def results(current_account, savings_account, credit_card, isa, mortgage, branch
     sorted_banks_and_scores = sorted(banks_and_scores, key=lambda x: x[1], reverse=True)
     '''print(sorted_banks_and_scores)'''
 
-    return render_template('results.html', sorted_banks_and_scores=sorted_banks_and_scores)
+    return render_template('results.html', sorted_banks_and_scores=sorted_banks_and_scores,
+                           sorted_banks_and_scores_services=sorted_banks_and_scores_services)
